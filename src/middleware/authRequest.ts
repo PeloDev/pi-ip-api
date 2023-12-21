07cc4b3148a189b2edcdc8c1321823bfa40da293
Boipelo Zuma <pelothedeveloper@gmail.com>
2023-12-21 13:10:13 +0200

    jwt: expire token after 5 mins from issuance

--- src/middleware/authRequest.ts
+++ src/middleware/authRequest.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import logger from "../services/loggerService";
+
+const TOKEN_EXPIRATION_PERIOD = 300; // 5 mins in seconds
 
 export const authenticateRequest = (
   req: Request,
@@ -9,16 +12,37 @@ export const authenticateRequest = (
   if (!process.env.JWT_SECRET) {
     throw new Error("Security credentials not found");
   }
-  
+
   const token = req.headers["authorization"]?.split(" ")[1]; // Bearer Token
 
-  if (token == null) return res.sendStatus(401);
+  if (!token) {
+    return res.sendStatus(401);
+  }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    const issuedAtTime = (decoded as jwt.JwtPayload)?.iat;
+
+    if (!issuedAtTime) {
+      logger.error(
+        "[middleware - authenticateRequest]: token has no issue time"
+      );
       return res.sendStatus(403);
     }
-    (req as Request & { user: string | JwtPayload | undefined }).user = user;
+
+    if (
+      Math.floor(Date.now() / 1000) >=
+      issuedAtTime + TOKEN_EXPIRATION_PERIOD
+    ) {
+      logger.error(
+        "[middleware - authenticateRequest]: token expired"
+      );
+      return res.sendStatus(403);
+    }
+
+    if (err) {
+      logger.error(`[middleware - authenticateRequest]: ${err.message}`);
+      return res.sendStatus(403);
+    }
     next();
   });
 };
